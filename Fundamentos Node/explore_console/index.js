e72b64d883a1b8f78e7aa9cd1d374834cc4a7497
Gabriel Souza <gablgdario03@gmail.com>
2023-07-05 13:52:33 -0300

    Learning about console

---
+++ Fundamentos Node/explore_console/index.js
@@ -0,0 +1,19 @@
+//mais de um valor
+const x= 10
+const y= 'Gabriel'
+const z= [1,2]
+
+console.log(x,y,z)
+
+//contagem de impressões
+console.count(`O valor de x é: ${x}, contagem`)
+console.count(`O valor de x é: ${x}, contagem`)
+console.count(`O valor de x é: ${x}, contagem`)
+
+// variavel enre string
+console.log('O nome é %s, e ele é programado',y)
+
+//limpar o console
+setTimeout(()=>{
+    console.clear()
+}, 2000)
