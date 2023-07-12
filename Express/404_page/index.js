42cc91057a3458e07fad7b0fd8ed04633195d445
Gabriel Souza <gablgdario03@gmail.com>
2023-07-12 14:56:18 -0300

    Add page 404

--- Express/404_page/index.js
+++ Express/404_page/index.js
@@ -39,6 +39,10 @@ app.get('/', (req,res)=>{
     res.sendFile(`${basePath}/index.html`)
 })
 
+app.use(function(req,res,next){
+    res.status(404).sendFile(`${basePath}/404.html`)
+})
+
 app.listen(port, ()=>{
     console.log(`Serve working in port ${port}`)
 })
