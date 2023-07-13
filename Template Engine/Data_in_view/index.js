3b1670a40dde8172eebed03db8344d05aca78f15
Gabriel Souza <gablgdario03@gmail.com>
2023-07-12 23:01:17 -0300

    Learning about Handlebars

---
+++ Template Engine/Data_in_view/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+app.get('/', (req,res)=>{
+    const user = {
+        name: 'Gabriel',
+        surname: 'Souza',
+        age: '22'
+    }
+
+    const palavra= "teste"
+    res.render('home',{user:user, palavra})
+})
+
+app.listen(3000, ()=>{
+    console.log('app rolando')
+})
