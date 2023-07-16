9818169fca2c5e27b36e3f26709719825a86d386
Gabriel Souza <gablgdario03@gmail.com>
2023-07-15 23:22:24 -0300

    create model

--- Sequelize/install_sequelize/db/conn.js
+++ Sequelize/install_sequelize/db/conn.js
@@ -5,12 +5,12 @@ const sequelize = new Sequelize('nodesequelize', 'root', 'Savitar18',{
     dialect: 'mysql'
 })
 
-try{
-    sequelize.authenticate()
-    console.log('Conectado com sucesso com o Sequelize!')
+// try{
+//     sequelize.authenticate()
+//     console.log('Conectado com sucesso com o Sequelize!')
 
-} catch(err){
-    console.log('Não foi possível conectar: ', err)
-}
+// } catch(err){
+//     console.log('Não foi possível conectar: ', err)
+// }
 
 module.exports = sequelize
