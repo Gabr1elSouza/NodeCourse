aec4af1665dc45f338b47ce1a3a20b17f3c2b22b
Gabriel Souza <gablgdario03@gmail.com>
2023-07-15 23:59:25 -0300

    insert

---
+++ Sequelize/insert/db/conn.js
@@ -0,0 +1,16 @@
+const { Sequelize} = require('sequelize')
+
+const sequelize = new Sequelize('nodesequelize', 'root', 'Savitar18',{
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+try{
+    sequelize.authenticate()
+    console.log('Conectado com sucesso com o Sequelize!')
+
+} catch(err){
+    console.log('Não foi possível conectar: ', err)
+}
+
+module.exports = sequelize
