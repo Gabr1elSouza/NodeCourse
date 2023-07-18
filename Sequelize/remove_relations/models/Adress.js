918e58c252599e77a6b76b0a3c531ed87eccece0
Gabriel Souza <gablgdario03@gmail.com>
2023-07-18 15:04:52 -0300

    remove address

---
+++ Sequelize/remove_relations/models/Adress.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize')
+
+const db = require('../db/conn')
+
+const User= require('./user')
+
+const Adress = db.define('Adress',{
+    street: {
+        type: DataTypes.STRING,
+        require: true
+    },
+    number: {
+        type: DataTypes.STRING,
+        require: true
+    },
+    city: {
+        type: DataTypes.STRING,
+        require: true
+    },
+
+})
+
+User.hasMany(Adress)
+Adress.belongsTo(User)
+
+module.exports = Adress
