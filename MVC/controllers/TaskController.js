d86a0b964ee185183cd94dde2ee552315807602f
Gabriel Souza <gablgdario03@gmail.com>
2023-07-20 13:46:48 -0300

    MVC PART 5 CREATE AND SELECT

--- MVC/controllers/TaskController.js
+++ MVC/controllers/TaskController.js
@@ -5,7 +5,24 @@ module.exports = class TaskController{
     static createTask(req,res){
         res.render('tasks/create')
     }
-    static showTasks(req,res){
-        res.render('tasks/all')
+    static async createTaskSave(req,res){
+         const task={
+            title: req.body.title,
+            description: req.body.description,
+            done:false
+         }
+
+         //validações
+         //Processar dados
+
+         await Task.create(task)
+
+         res.redirect('/tasks')
     }
+    static async showTasks(req,res){
+        const tasks = await Task.findAll({raw: true})
+
+        res.render('tasks/all',{tasks})
+    }
+
 }
