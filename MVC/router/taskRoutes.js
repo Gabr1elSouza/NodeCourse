07d3a840accf5d72558fe23e98d8ac1cba9c2a69
Gabriel Souza <gablgdario03@gmail.com>
2023-07-21 15:59:04 -0300

    MVC PART 5 DELETE

--- MVC/router/taskRoutes.js
+++ MVC/router/taskRoutes.js
@@ -6,5 +6,6 @@ const TaskController = require ('../controllers/TaskController')
 router.get('/add', TaskController.createTask)
 router.post('/add', TaskController.createTaskSave)
 router.get('/', TaskController.showTasks)
+router.post('/remove', TaskController.removeTask)
 
 module.exports = router
--- Node Core Modules/work_with_folders/index.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-
-if(!fs.existsSync('./minhapasta')){
-    console.log('Não existe esse diretorio')
-    fs.mkdirSync('minhapasta')
-} else if(fs.existsSync('./minhapasta')){
-    console.log('Existe esse diretorio')
-}
