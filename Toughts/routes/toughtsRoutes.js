c7bcaba97ca72b8405bcbf27dd162131aca0d293
Gabriel Souza <gablgdario03@gmail.com>
2023-07-31 16:05:21 -0300

    PROJECT TOUGHT PART 14

--- Toughts/routes/toughtsRoutes.js
+++ Toughts/routes/toughtsRoutes.js
@@ -6,6 +6,8 @@ const ToughtsController = require('../controllers/ToughtController')
 const checkAuth =  require('../helpers/auth').checkAuth
 
 router.get('/add',checkAuth,ToughtsController.createTought)
+router.get('/edit/:id',checkAuth,ToughtsController.updateTought)
+router.post('/edit/',checkAuth,ToughtsController.updateToughtSave)
 router.post('/add',checkAuth,ToughtsController.createToughtSave)
 router.get('/dashboard',checkAuth,ToughtsController.dashboard)
 router.post('/remove', checkAuth, ToughtsController.removeTought)
