58b5da217f2fd66d94be5768cefa66859c324be2
Gabriel Souza <gablgdario03@gmail.com>
2023-08-09 07:48:50 -0300

    Mongoose Insert

--- Mongoose/routes/productsRoutes.js
+++ Mongoose/routes/productsRoutes.js
@@ -4,11 +4,11 @@ const ProductController = require('../controllers/ProductController')
 
 router.get('/create', ProductController.createProduct)
 router.post('/create', ProductController.createProductPost)
-router.post('/remove/:id', ProductController.removeProduct)
-router.get('/:id', ProductController.getProduct)
-router.get('/edit/:id', ProductController.editProduct)
-router.post('/edit', ProductController.editProductPost)
-router.get('/', ProductController.showProducts)
+// router.post('/remove/:id', ProductController.removeProduct)
+// router.get('/:id', ProductController.getProduct)
+// router.get('/edit/:id', ProductController.editProduct)
+// router.post('/edit', ProductController.editProductPost)
+// router.get('/', ProductController.showProducts)
 
 
 module.exports = router
