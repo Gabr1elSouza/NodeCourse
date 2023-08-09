607c36713f0fe11255f17ac2cd6b9c8317f21f3c
Gabriel Souza <gablgdario03@gmail.com>
2023-08-09 07:39:33 -0300

    Mongoose Schema

--- Mongoose/models/Product.js
+++ Mongoose/models/Product.js
@@ -1,51 +1,15 @@
-const conn = require('../db/conn')
+const mongoose = require('mongoose')
 
-const { ObjectId } = require('mongodb')
-class Product{
-    constructor(name,image, price, description){
-        this.name = name
-        this.image = image
-        this.price = price
-        this.description = description
-    }
+const ProductSchema = new mongoose.Schema({
+        name: { type: String, required: true},
+        price: {type: Number, required: true},
+        description: {type: String, required: true},
+        image:{type: String, required: true}
+    })
 
-    save(){
-        const product = conn.db().collection('products').insertOne({
-            name: this.name,
-            image: this.image,
-            price: this.price,
-            description: this.description
-        })
-        return product
-    }
 
-    static getProducts(){
-        const products = conn.db().collection('products').find().toArray()
+const Product = mongoose.model('Product', ProductSchema)
 
-        return products
-    }
 
-    static async getProductById(id){
-
-        const product = await conn.db().collection('products').findOne({ _id: new ObjectId(id)})
-
-        return product
-    }
-
-    static async removeProductById(id){
-        await conn.db().collection('products').deleteOne({_id: new ObjectId(id)})
-
-        return
-    }
-
-    static async updateProduct(id,product) {
-        await conn
-            .db()
-            .collection('products')
-            .updateOne({ _id: new ObjectId(id)}, {$set: product});
-    
-        return
-    }    
-}
 
 module.exports = Product
