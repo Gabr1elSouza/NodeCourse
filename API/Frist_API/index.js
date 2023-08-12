4181dbf522d81d4fc1c4bf89852af68643cbdf2b
Gabriel Souza <gablgdario03@gmail.com>
2023-08-11 23:59:34 -0300

    Frist API

---
+++ API/Frist_API/index.js
@@ -0,0 +1,13 @@
+const express= require('express')
+const app = express()
+
+app.use(express.urlencoded({extended:true}))
+
+app.use(express.json())
+
+//routes - endpoints
+app.get('/',(req,res)=>{
+    res.json({message: 'Hello World!'})
+})
+
+app.listen(3000)
