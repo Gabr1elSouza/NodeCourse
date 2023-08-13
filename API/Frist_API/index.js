25f43ebb6da13e9c1f7604b82aca4f0bdf66c84b
Gabriel Souza <gablgdario03@gmail.com>
2023-08-12 23:08:07 -0300

    use post verb in Postman

--- API/Frist_API/index.js
+++ API/Frist_API/index.js
@@ -6,6 +6,16 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
 //routes - endpoints
+app.post('/createproduct',(req,res)=>{
+    const name= req.body.name
+    const price = req.body.price
+
+    console.log(name)
+    console.log(price)
+
+    res.json({message:`O produto ${name} foi criado com sucesso!`})
+})
+
 app.get('/',(req,res)=>{
     res.json({message: 'Hello World!'})
 })
