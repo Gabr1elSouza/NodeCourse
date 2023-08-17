8282874c2ca9a35264a13572f8df5497ec5a6421
Gabriel Souza <gablgdario03@gmail.com>
2023-08-17 16:09:33 -0300

    Get a pet PART 8

--- Get a Pet/backend/controllers/UserController.js
+++ Get a Pet/backend/controllers/UserController.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken')
 //helpers
 const createUserToken = require('../helpers/create-user-token')
 const getToken = require('../helpers/GetToken')
+const getUserByToken = require('../helpers/get-user-by-token')
 
 module.exports = class UserController{
     static async register(req,res){
@@ -31,7 +32,7 @@ module.exports = class UserController{
             return
         }
         if(password !== confirmpassword){
-            res.status(422).json({message: 'A senha e a confirmação de senha é precisa ser iguais'})
+            res.status(422).json({message: 'A senha e a confirmação de senha precisa ser iguais'})
             return
         }
 
@@ -121,7 +122,65 @@ module.exports = class UserController{
     }
 
     static async editUser(req,res){
-        res.status(200).json({message: 'Deu certo update!'})
+        const id = req.params.id
+
+        //check if user exists
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        const {name,email,phone,password,confirmpassword}=req.body
+
+        if(req.file){
+            user.image = req.file.filename
+        }
+
+        //validations
+        if(!name){
+            res.status(422).json({message: 'O nome é obrigatório'})
             return
+        }
+        user.name = name
+        if(!email){
+            res.status(422).json({message: 'O e-mail é obrigatório'})
+            return
+        }
+        //check if  email has already taken
+        const userExists = await User.findOne({email:email})
+        if(user.email !== email && userExists){
+            res.status(422).json({message: 'por favor utilize outro email'})
+            
+        }
+
+        user.email = email
+
+        if(!phone){
+            res.status(422).json({message: 'O telefone é obrigatório'})
+            return
+        }
+        user.phone = phone
+
+        if(password !== confirmpassword){
+            res.status(422).json({message: 'As senhas não conferem'})
+            return
+        }else if(password === confirmpassword && password != null){
+            
+            //creating password
+            const salt = await bcrypt.genSalt(12)
+            const passwordHash = await bcrypt.hash(password,salt)
+
+            user.password = passwordHash
+        }
+        
+        try {
+            //return user update data
+            await User.findByIdAndUpdate({_id:user._id},{$set:user},{new:true})
+
+            res.status(200).json({message: "Usuário atualizado com sucesso!"})
+
+        } catch (err) {
+            res.status(500).json({message: err})
+            return
+        }
+
     }
 }
