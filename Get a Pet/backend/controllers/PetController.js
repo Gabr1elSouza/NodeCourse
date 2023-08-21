7326b229566b8c4788f55d67e4e830c1219b6b47
Gabriel Souza <gablgdario03@gmail.com>
2023-08-20 22:47:46 -0300

    Get a pet PART 11

--- Get a Pet/backend/controllers/PetController.js
+++ Get a Pet/backend/controllers/PetController.js
@@ -69,4 +69,9 @@ module.exports = class PetController{
             
         }
     }
+    
+    static async getAll(req,res){
+        const pets = await Pet.find().sort('-createdAt')
+        res.status(200).json({pets: pets})
+    }
 }
--- Get a Pet/backend/routes/petRoutes.js
+++ Get a Pet/backend/routes/petRoutes.js
@@ -6,5 +6,6 @@ const verifyToken = require('../helpers/verify-Token')
 const {imageUpload} = require('../helpers/image-upload')
 
 router.post('/create',verifyToken, imageUpload.array('images'), PetController.create)
+router.get('/', PetController.getAll)
 
 module.exports = router
