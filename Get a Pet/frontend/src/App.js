c33b9ed0cefa39f98783f49e9b65034c214b0c43
Gabriel Souza <gablgdario03@gmail.com>
2023-08-27 00:34:11 -0300

    Get a pet PART 21

--- Get a Pet/frontend/src/App.js
+++ Get a Pet/frontend/src/App.js
@@ -1,10 +1,25 @@
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
+/*Components */
+import Navbar from './components/Layout/Navbar'
+import Footer from './components/Layout/Footer'
+
+/* PAGES*/
+import Login from './components/pages/auth/Login'
+import Home from './components/pages/Home'
+import Register from './components/pages/auth/Register'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Get A Pet</h1>
-    </div>
+    <Router>
+      <Navbar/>
+    <Routes>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
+    <Footer/>
+    </Router>
   );
 }
 
