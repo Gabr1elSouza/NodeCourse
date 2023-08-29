9e9d7f4311eb2c9f27a682af3c1ccf909732b344
Gabriel Souza <gablgdario03@gmail.com>
2023-08-29 17:27:46 -0300

    Get a pet PART 26

--- Get a Pet/frontend/src/hooks/useAuth.js
+++ Get a Pet/frontend/src/hooks/useAuth.js
@@ -2,11 +2,23 @@
 import  api from '../utils/api'
 
 import { useState, useEffect } from 'react'
-import{ useHistory } from 'react-router-dom'
+import{ useNavigate } from 'react-router-dom'
 import useFlashMessage from './userFlashMessage'
 
 export default function useAuth(){
+    const [authenticated, setAuthenticated] =useState(false)
     const {setFlashMessage} = useFlashMessage()
+    const history = useNavigate()
+
+    useEffect(()=>{
+        const token = localStorage.getItem('token')
+
+        if(token){
+            api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`
+            setAuthenticated(true)
+        }
+    }, [])
+
     async function register(user){
         let msgText = 'Cadastro realizado com sucesso!'
         let msgType = 'success'
@@ -15,12 +27,18 @@ export default function useAuth(){
             const data = await api.post('/users/register',user).then((response)=>{
                 return response.data
             })
-            console.log(data)
+            await authUser(data)
         } catch (err) {
             msgText = err.response.data.message
             msgType = 'error'
         }
         setFlashMessage(msgText, msgType)
     }
-    return { register }
+
+    async function authUser(data){
+        setAuthenticated(true)
+        localStorage.setItem('token',JSON.stringify(data.token))
+        history('/')
+    }
+    return { authenticated, register }
 }
