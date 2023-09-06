994d5fae0c849cc63581229054d79c7ce52672db
Gabriel Souza <gablgdario03@gmail.com>
2023-09-05 22:23:51 -0300

    Get a pet PART 36

--- Get a Pet/frontend/src/components/pages/Pets/Mypets.js
+++ Get a Pet/frontend/src/components/pages/Pets/Mypets.js
@@ -27,26 +27,26 @@ function Mypets(){
     },[token])
 
     return(
-        <section>
-            <div>
+        <section >
+            <div className={styles.petlist_header}>
             <h1>Mypets</h1>
             <Link to="/pet/add">Cadastrar Pet</Link>
             </div>
-            <div>
+            <div className={styles.petlist_container}>
                 {pets.length > 0 && 
                     pets.map((pet) => (
-                        <div key={pet.id}>
+                        <div className={styles.petlist_row} key={pet.id}>
                             <RoundedImage
                             src={`${process.env.REACT_APP_API}/images/pets/${pet.images[0]}`}
                             alt={pet.name}
-                            width="75px"
+                            width="px75"
                             />
                             <span className='bold'>{pet.name}</span>
-                            <div className={StyleSheet.actions}>
+                            <div className={styles.actions}>
                              {pet.available ? (
                                 <>
                                 {pet.adopter && (
-                                    <button>Concluir adoção</button>
+                                    <button className={styles.conclude_btn}>Concluir adoção</button>
                                 )}
                                 <Link to={`/pet/edit/${pet._id}`} >Editar</Link>
                                 <button>Excluir</button>
